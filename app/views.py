4d602b8bfa4e5400deeee0a7ae404b5a30f29f51
jspmern <jspmern@gmail.com>
2024-06-19 10:37:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def srujana(request):
+    return HttpResponse('<h1><marquee>Srujana Tinnavara,Era Vadilastunnavaaaa......<marquee></h1>')
+
+def meghana(request):
+    return HttpResponse('<h1><marquee>My Name is Meghanaaa Sir......<marquee></h1>')
+    
